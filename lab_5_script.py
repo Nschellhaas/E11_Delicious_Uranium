ab6af050e3f8979247a3091aba46f3e80f81ec0e
Nschellhaas <nathan_schellhaas@berkeley.edu>
2025-02-19 15:04:22 -0800

    Update lab_5_script.py

--- lab_5_script.py
+++ lab_5_script.py
@@ -75,53 +75,58 @@ else:
 print("Found PM2.5 sensor, reading data...")
 
 while time_now < run_time:
-    time.sleep(1)
-    time_now += 1
-  
-
-    try:
-        aqdata = pm25.read()
-        # print(aqdata)
-    except RuntimeError:
-        print("Unable to read from sensor, retrying...")
-        continue
-
-
-    
-    print()
-    print("Concentration Units (standard)")
-    print("---------------------------------------")
-    print("Time (s):" + str(time_now))
-    print("---------------------------------------")
-    print(
-        "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
-        % (aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"])
-    )
-    print("Concentration Units (environmental)")
-    print("---------------------------------------")
-    print(
-        "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
-        % (aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"])
-    )
-    print("---------------------------------------")
-    print("Particles > 0.3um / 0.1L air:", aqdata["particles 03um"])
-    print("Particles > 0.5um / 0.1L air:", aqdata["particles 05um"])
-    print("Particles > 1.0um / 0.1L air:", aqdata["particles 10um"])
-    print("Particles > 2.5um / 0.1L air:", aqdata["particles 25um"])
-    print("Particles > 5.0um / 0.1L air:", aqdata["particles 50um"])
-    print("Particles > 10 um / 0.1L air:", aqdata["particles 100um"])
-    print("---------------------------------------")
 	
-    time = time.time() 
-    temp = bme680.temperature
-    gas = bme680.gas
-    pres = bme680.pressure
-    alt = bme680.altitude
-    hum = bme680.relative_humidity
+	time.sleep(1)
+	time_now += 1
+	  
 	
-
+	try:
+	        aqdata = pm25.read()
+	        # print(aqdata)
+	except RuntimeError:
+	        print("Unable to read from sensor, retrying...")
+	        continue
 	
+	
+	    
+	print()
+	print("Concentration Units (standard)")
+	print("---------------------------------------")
+	print("Time (s):" + str(time_now))
+	print("---------------------------------------")
+	print(
+	        "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
+	        % (aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"])
+	    )
+	print("Concentration Units (environmental)")
+	print("---------------------------------------")
+	print(
+	        "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
+	        % (aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"])
+	    )
+	print("---------------------------------------")
+	print("Particles > 0.3um / 0.1L air:", aqdata["particles 03um"])
+	print("Particles > 0.5um / 0.1L air:", aqdata["particles 05um"])
+	print("Particles > 1.0um / 0.1L air:", aqdata["particles 10um"])
+	print("Particles > 2.5um / 0.1L air:", aqdata["particles 25um"])
+	print("Particles > 5.0um / 0.1L air:", aqdata["particles 50um"])
+	print("Particles > 10 um / 0.1L air:", aqdata["particles 100um"])
+	print("---------------------------------------")
+		
+	time = time.time() 
+	temp = bme680.temperature
+	gas = bme680.gas
+	pres = bme680.pressure
+	alt = bme680.altitude
+	hum = bme680.relative_humidity
 
-    csvwriter.writerow([time_now, aqdata["particles 25um"], temp, gas, pres, alt, hum])
+	print("Temperature", temp)
+	print("Gas", gas)
+	print("Pressure", pres)
+	print("Altitude", alt)
+	print("Humidity", hum)
+		
+	
+	csvwriter.writerow([time_now, aqdata["particles 25um"], temp, gas, pres, alt, hum])
 
 file.close()   
