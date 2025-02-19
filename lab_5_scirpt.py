9a33195cbbf5f9983d223b1ece4edfe840519d8f
Nschellhaas <nathan_schellhaas@berkeley.edu>
2025-02-19 14:50:24 -0800

    Create lab_5_scirpt.py
    combined lab 3 & 4 scripts

---
+++ lab_5_scirpt.py
@@ -0,0 +1,123 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+"""
+Example sketch to connect to PM2.5 sensor with either I2C or UART.
+"""
+
+# pylint: disable=unused-import
+import time
+import board
+import busio
+import csv
+import sys
+import adafruit_bme680
+from digitalio import DigitalInOut, Direction, Pull
+from adafruit_pm25.i2c import PM25_I2C
+
+n = 0
+# Create sensor object, communicating over the board's default I2C bus
+i2c = board.I2C()   # uses board.SCL and board.SDA
+bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
+
+# change this to match the location's pressure (hPa) at sea level
+bme680.sea_level_pressure = 1013.25
+
+weather_data = np.zeros((1,6))
+
+
+reset_pin = None
+# If you have a GPIO, its not a bad idea to connect it to the RESET pin
+# reset_pin = DigitalInOut(board.G0)
+# reset_pin.direction = Direction.OUTPUT
+# reset_pin.value = False
+
+
+# For use with a computer running Windows:
+# import serial
+# uart = serial.Serial("COM30", baudrate=9600, timeout=1)
+
+# For use with microcontroller board:
+# (Connect the sensor TX pin to the board/computer RX pin)
+#uart = busio.UART(board.TX, board.RX, baudrate=9600)
+
+# For use with Raspberry Pi/Linux:
+import serial
+uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
+
+# For use with USB-to-serial cable:
+# import serial
+# uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=0.25)
+
+# Connect to a PM2.5 sensor over UART
+from adafruit_pm25.uart import PM25_UART
+pm25 = PM25_UART(uart, reset_pin)
+
+# Create library object, use 'slow' 100KHz frequency!
+#i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
+# Connect to a PM2.5 sensor over I2C
+#pm25 = PM25_I2C(i2c, reset_pin)
+
+file = open('Lab_4_Data.csv', 'w', newline = None)
+
+csvwriter = csv.writer(file, delimiter = ',')
+
+csvwriter.writerow(["Time", "Particles > 2.5um / 0.1L air", "Temperature", "Gas", "Pressure", "Altitude", "Humidity"])
+
+time_now = 0
+
+if len(sys.argv) < 2:
+    run_time = 30
+else:
+    run_time = int(sys.argv[1])
+
+print("Found PM2.5 sensor, reading data...")
+
+while time_now <: run_time
+    time.sleep(1)
+    time_now += 1
+  
+
+    try:
+        aqdata = pm25.read()
+        # print(aqdata)
+    except RuntimeError:
+        print("Unable to read from sensor, retrying...")
+        continue
+
+
+    
+    print()
+    print("Concentration Units (standard)")
+    print("---------------------------------------")
+    print("Time (s):" + str(time_now))
+    print("---------------------------------------")
+    print(
+        "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
+        % (aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"])
+    )
+    print("Concentration Units (environmental)")
+    print("---------------------------------------")
+    print(
+        "PM 1.0: %d\tPM2.5: %d\tPM10: %d"
+        % (aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"])
+    )
+    print("---------------------------------------")
+    print("Particles > 0.3um / 0.1L air:", aqdata["particles 03um"])
+    print("Particles > 0.5um / 0.1L air:", aqdata["particles 05um"])
+    print("Particles > 1.0um / 0.1L air:", aqdata["particles 10um"])
+    print("Particles > 2.5um / 0.1L air:", aqdata["particles 25um"])
+    print("Particles > 5.0um / 0.1L air:", aqdata["particles 50um"])
+    print("Particles > 10 um / 0.1L air:", aqdata["particles 100um"])
+    print("---------------------------------------")
+
+    time = time.time()
+	  temp = bme680.temperature
+  	gas = bme680.gas
+	  pres = bme680.pressure
+	  alt = bme680.altitude
+	  hum = bme680.relative_humidity
+
+    csvwriter.writerow([time_now, aqdata["particles 25um"], temp, gas, pres, alt, hum])
+
+file.close()   
