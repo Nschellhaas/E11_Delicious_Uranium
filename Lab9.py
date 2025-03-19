e7dac89de3c4bdeb5ca4a88a9ad48cd848cebf56
Shay Ellis <shaye6639@gmail.com>
2025-03-19 16:26:07 -0700

    Update Lab9.py
    Update

--- Lab9.py
+++ Lab9.py
@@ -9,15 +9,19 @@ def my_callback(channel):
         print(datetime.datetime.now())
         print(count)
         count+=1
+        return count
 
 
 global count
 count=0
 channel=17
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(channel,GPIO.IN)
-GPIO.setup(channel,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(channel,GPIO.FALLING,callback=my_callback)
-while True:
-    time.sleep(10)
+try:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(channel,GPIO.IN)
+    GPIO.setup(channel,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(channel,GPIO.FALLING,callback=my_callback)
+finally:
+    GPIO.cleanup()
+#while True:
+#    time.sleep(10)
 
