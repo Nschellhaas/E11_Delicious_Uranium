f8bb12bfad7bbad089b9f88793ca39d96a2e6281
Shay Ellis <shaye6639@gmail.com>
2025-03-19 16:32:26 -0700

    Update Lab9.py
    Update

--- Lab9.py
+++ Lab9.py
@@ -3,25 +3,23 @@ import RPi.GPIO as GPIO # type: ignore
 import datetime
 import time
 
+
+
 def my_callback(channel):
     global count
-    if GPIO.event_detected(channel):
-        print(datetime.datetime.now())
-        print(count)
-        count+=1
-        return count
+    print(datetime.datetime.now())
+    print(count)
+    count+=1
 
 
 global count
 count=0
+global channel
 channel=17
-try:
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(channel,GPIO.IN)
-    GPIO.setup(channel,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-    GPIO.add_event_detect(channel,GPIO.FALLING,callback=my_callback)
-finally:
-    GPIO.cleanup()
-#while True:
-#    time.sleep(10)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel,GPIO.IN)
+GPIO.setup(channel,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(channel,GPIO.FALLING,callback=my_callback)
+while True:
+    time.sleep(10)
 
