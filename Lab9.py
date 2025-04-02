87174227af1ca56ffb7f305aa4bc1da766ea022a
Shay Ellis <shaye6639@gmail.com>
2025-04-02 14:45:41 -0700

    Update Lab9.py

--- Lab9.py
+++ Lab9.py
@@ -1,15 +1,24 @@
 import numpy as np # type: ignore
 import RPi.GPIO as GPIO # type: ignore
+import csv
 import datetime
 import time
     
 
+global sec
+file = open('Lab10_Data.csv', 'w', newline = None)
 
+csvwriter = csv.writer(file, delimiter = ',')
+
+csvwriter.writerow(["Time", "Count"])
+base = datetime.datetime.now().second
 def my_callback(channel):
     global count
-    print(datetime.datetime.now())
-    print(count)
+    t=datetime.datetime.now().second
     count+=1
+    print("f.Time: {t}")
+    print("f.Count: {count}")
+    csvwriter.writerow([t,count])
 
 
 global count
@@ -20,6 +29,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel,GPIO.IN)
 GPIO.setup(channel,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(channel,GPIO.FALLING,callback=my_callback)
-while True:
+while (datetime.datetime.now().second-base)<=20:
     time.sleep(10)
 
+file.close()
